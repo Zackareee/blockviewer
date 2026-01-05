68c17eac38679d307c592ef77195bc0044381999
Zackaree <zackaree@github.com>
2026-01-05 16:20:44 +1000

    Disable Vite terminal clearing

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      // Don't clear browser console on fast refresh
+      fastRefresh: {
+        // Disable console clearing
+        include: '**/*.{jsx,tsx}',
+      },
+    }),
+  ],
+  // Don't clear terminal on HMR updates
+  clearScreen: false,
   // Serve textures folder as additional public assets
   publicDir: 'public',
   // Enable SharedArrayBuffer support with COOP/COEP headers
